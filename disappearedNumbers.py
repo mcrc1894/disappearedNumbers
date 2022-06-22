6e3efec1b9b271c95cf33794fded639639e0cad1
mcrc1894 <76842196+mcrc1894@users.noreply.github.com>
2022-06-22 08:55:38 +0700

    Update disappearedNumbers.py

--- disappearedNumbers.py
+++ disappearedNumbers.py
@@ -1,4 +1,4 @@
 arr = []
 def disappeared_numbers(a):
-    arr=set(a)
-    return [i for i in range(1,len(a)+1) if i not in arr]
+    arr = set(a)
+    return [i for i in range(1, len(a)+1) if i not in arr]
